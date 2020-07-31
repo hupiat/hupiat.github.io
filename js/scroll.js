b32a8e0a2fb4d0ddb27c8efd23e172fc5d929327
Hugo Piat <h.piat@outlook.fr>
2020-07-31 23:07:02 +0200

    Adding scroll to top button

---
+++ js/scroll.js
@@ -0,0 +1,52 @@
+const ANIM_DURATION_MS = 50;
+const TRIGGER_ANCHOR_PX = document.body.offsetWidth < 1280 ? 800 : 100;
+const DEBOUNCE_SCROLL_DELAY_MS = 200;
+
+const getBodyScrollTop = () => {
+  const el = document.scrollingElement || document.documentElement;
+  return el.scrollTop;
+};
+
+let scroll_timeout;
+let will_change_visiblity = false;
+const arrow = document.getElementById("up-arrow");
+
+const scroll_callback = () => {
+  if (will_change_visiblity) {
+    return;
+  }
+  if (scroll_timeout) {
+    clearTimeout(scroll_timeout);
+  }
+  const scroll_top = getBodyScrollTop();
+  const opacity = Number(arrow.style.opacity);
+  if (
+    (scroll_top > TRIGGER_ANCHOR_PX && opacity === 0) ||
+    (scroll_top < TRIGGER_ANCHOR_PX && opacity === 1)
+  ) {
+    will_change_visiblity = true;
+  }
+  if (will_change_visiblity) {
+    scroll_timeout = setTimeout(() => {
+      Velocity(
+        arrow,
+        {
+          opacity: scroll_top > TRIGGER_ANCHOR_PX ? 1 : 0,
+        },
+        {
+          duration: ANIM_DURATION_MS,
+        }
+      );
+      will_change_visiblity = false;
+    }, DEBOUNCE_SCROLL_DELAY_MS);
+  }
+};
+
+document.getElementById("main").addEventListener("scroll", scroll_callback);
+document.addEventListener("scroll", scroll_callback);
+
+arrow.addEventListener("click", () => {
+  Velocity(document.body, {
+    scrollTop: 0,
+  });
+});
