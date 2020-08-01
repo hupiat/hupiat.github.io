e1cb657c246d3b8f7106d0542c639095c7c2a5af
Hugo Piat <h.piat@outlook.fr>
2020-08-01 05:20:32 +0200

    Renaming into camelCase

--- js/scroll.js
+++ js/scroll.js
@@ -21,17 +21,17 @@ let triggerAnchorPos =
 
 const arrow = document.getElementById("up-arrow");
 
-const scroll_callback = () => {
+const scrollCallback = () => {
   if (willChangeVisibility) {
     return;
   }
   if (scrollTimeout) {
     clearTimeout(scrollTimeout);
   }
-  const scroll_top = getScrollValue();
+  const scrollTop = getScrollValue();
   if (
-    (scroll_top > triggerAnchorPos && !isVisible) ||
-    (scroll_top < triggerAnchorPos && isVisible)
+    (scrollTop > triggerAnchorPos && !isVisible) ||
+    (scrollTop < triggerAnchorPos && isVisible)
   ) {
     willChangeVisibility = true;
   }
@@ -41,7 +41,7 @@ const scroll_callback = () => {
         arrow,
         {
           right:
-            scroll_top > triggerAnchorPos
+            scrollTop > triggerAnchorPos
               ? UP_ARROW_VISIBLE_RIGHT_PX
               : UP_ARROW_HIDDEN_RIGHT_PX,
         },
@@ -49,14 +49,14 @@ const scroll_callback = () => {
           duration: ANIM_DURATION_MS,
         }
       );
-      isVisible = scroll_top > triggerAnchorPos;
+      isVisible = scrollTop > triggerAnchorPos;
       willChangeVisibility = false;
     }, DEBOUNCE_DELAY_MS);
   }
 };
 
-document.getElementById("main").addEventListener("scroll", scroll_callback);
-document.addEventListener("scroll", scroll_callback);
+document.getElementById("main").addEventListener("scroll", scrollCallback);
+document.addEventListener("scroll", scrollCallback);
 
 let resizeTimeout;
 window.addEventListener("resize", () => {
@@ -68,7 +68,7 @@ window.addEventListener("resize", () => {
       document.body.offsetWidth < MAX_WIDTH_MOBILE_PX
         ? TRIGGER_SCROLL_MOBILE_PX
         : TRIGGER_SCROLL_DESKTOP_PX;
-    scroll_callback();
+    scrollCallback();
   }, DEBOUNCE_DELAY_MS);
 });
 
