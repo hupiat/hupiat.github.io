dcc40dd615c637f4a45570b2406af8d2291ffd87
Hugo Piat <h.piat@outlook.fr>
2020-08-10 20:31:25 +0200

    Some fixes

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,7 @@
 const BUTTON_ANIM_DURATION_MS = 150;
 const DEBOUNCE_DELAY_MS = 300;
 const MAX_WIDTH_MOBILE_PX = 1280;
-const TRIGGER_SCROLL_MOBILE_PX = 800;
+const TRIGGER_SCROLL_MOBILE_PX = 500;
 const TRIGGER_SCROLL_DESKTOP_PX = 100;
 const UP_ARROW_VISIBLE_RIGHT_PX = 50;
 const UP_ARROW_HIDDEN_RIGHT_PX = -100;
@@ -84,7 +84,7 @@ arrow.addEventListener("click", () => {
     },
     {
       easing: "ease-in-out",
-      duration: getScrollValue(),
+      duration: getScrollValue() / 3,
     }
   );
 });
