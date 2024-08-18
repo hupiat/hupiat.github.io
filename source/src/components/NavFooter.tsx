869834ea0fe938c37635c5f85f40fad7da0455bf
Hugo Piat <h.piat@outlook.fr>
2024-08-18 02:52:28 +0200

    fixed footer styles + build

--- source/src/components/NavFooter.tsx
+++ source/src/components/NavFooter.tsx
@@ -1,5 +1,6 @@
 import { Carousel } from "antd";
 import React from "react";
+import { COLOR_PRIMARY } from "../utils/constants";
 
 interface IProps {
     imagePaths: string[];
@@ -9,10 +10,17 @@ interface IProps {
 export default function NavFooter({ imagePaths, imageAlts } : IProps) {
 
     return (    
-        <Carousel infinite={false} autoplay>
+        <Carousel infinite={false} autoplay style={{
+            marginTop: "15vh",
+            borderTop: "5px solid " + COLOR_PRIMARY,
+            paddingTop: "5vh"
+        }}>
             {
                 imagePaths.map((img, i) => <div key={i}>
-                    <img src={img} alt={imageAlts[i]} />
+                    <img src={img} alt={imageAlts[i]} style={{
+                    width: "300px",
+                    margin: "auto"
+                }} />
                 </div>)
             }
         </Carousel>
